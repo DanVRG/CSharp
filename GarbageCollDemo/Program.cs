c1cdfe43a2bc38ecae71bf74373bb4f8b42330c4
IAQACH <IAQACH@I417-10>
2022-09-15 09:09:15 +0200

    Add project files.

---
+++ GarbageCollDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GarbageCollDemo
+{
+    class TestClass
+    {
+        static int numOfInstances = 0;
+        public TestClass()
+        {
+            numOfInstances++;
+        }
+        ~TestClass()
+        {
+            numOfInstances--;
+        }
+        public int getNumOfInstances()
+        {
+            return numOfInstances;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            while (true)
+            {
+                TestClass tc = new TestClass();
+                if (max < tc.getNumOfInstances())
+                    max = tc.getNumOfInstances();
+                else
+                    break;
+                Console.WriteLine(tc.getNumOfInstances());
+            }
+            Console.WriteLine("Max példányszám: "+max);
+            Console.ReadLine();
+        }
+    }
+}
